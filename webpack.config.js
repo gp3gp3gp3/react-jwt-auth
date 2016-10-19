6113fe3e0155ed5057b2b82f7223b23dc7d38fc2
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-19 18:34:02 -0400

    Change public path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/public'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '/public/bundle.js'
   },
   module: {
     preLoaders: [
