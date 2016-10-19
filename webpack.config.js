29851f522718d1eeaff0ec8e8c5662fdfb0c475d
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-19 11:37:13 -0400

    initialize with packages/lockfile/linters/etc

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
