9c7a289a32f571a934d0f4b8ab6f8b0dbc6cd80d
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-24 15:04:17 -0400

    add dev webpack config file

---
+++ webpack.dev.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
