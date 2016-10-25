572406dea52bea72d4556a3188b2c852d8027b82
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-25 12:28:26 -0400

    Start building out root app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Header from './Header'
 
-const App = () => <div><h1>Welcome page</h1></div>
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  children: React.PropTypes.object
+}
 
 export default App
