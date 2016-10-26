242abd92a997cf93b4df901f9e2ffa881eb996b3
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-26 17:24:21 -0400

    Import signout component in root js

--- src/index.js
+++ src/index.js
@@ -8,18 +8,26 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import App from './components/App'
 import Welcome from './components/Welcome'
 import Signup from './components/auth/Signup'
+import Signout from './components/auth/Signout'
+import { AUTH_USER } from './actions/types'
 import reducers from './reducers'
 import './styles/app.scss'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
+const token = localStorage.getItem('token')
+if (token) {
+  store.dispatch({ type: AUTH_USER })
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <IndexRoute component={Welcome} />
-        <Route path='/signup' component={Signup} />
+        <Route path='signup' component={Signup} />
+        <Route path='signout' component={Signout} />
       </Route>
     </Router>
   </Provider>,
