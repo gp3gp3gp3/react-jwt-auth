fd8a957482bb540bb9ba9885198f680baa29b984
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-31 15:26:18 -0400

    Change browserHistory to hashHistory for temp fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { browserHistory } from 'react-router'
+import { hashHistory } from 'react-router'
 import {
   AUTH_USER,
   UNAUTH_USER,
@@ -15,7 +15,7 @@ export function signinUser ({ email, password }) {
     .then(response => {
       dispatch({ type: AUTH_USER })
       localStorage.setItem('token', response.data.token)
-      browserHistory.push('/feature')
+      hashHistory.push('/feature')
     })
     .catch(() => {
       dispatch(authError('Bad Login Info'))
@@ -29,7 +29,7 @@ export function signupUser ({ email, password }) {
     .then(response => {
       dispatch({ type: AUTH_USER })
       localStorage.setItem('token', response.data.token)
-      browserHistory.push('/feature')
+      hashHistory.push('/feature')
     })
     .catch(response => dispatch(authError(response.data.error)))
   }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom'
 import reduxThunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import App from './components/App'
 import Welcome from './components/Welcome'
 import Signin from './components/auth/Signin'
 import Signup from './components/auth/Signup'
 import Signout from './components/auth/Signout'
+import RequireAuth from './components/auth/RequireAuth'
+import Feature from './components/Feature'
 import { AUTH_USER } from './actions/types'
 import reducers from './reducers'
 import './styles/app.scss'
@@ -24,12 +26,13 @@ if (token) {
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path='/' component={App}>
         <IndexRoute component={Welcome} />
         <Route path='signin' component={Signin} />
         <Route path='signup' component={Signup} />
         <Route path='signout' component={Signout} />
+        <Route path='feature' component={RequireAuth(Feature)} />
       </Route>
     </Router>
   </Provider>,
