2321bb202687b8ff104d3cd859c84c4750166490
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-03 17:43:55 -0400

    Add event listener for toggling task to editing, no form

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -3,18 +3,29 @@ import React, { Component } from 'react'
 class Task extends Component {
 
   render () {
-    const { task, onDelClick } = this.props
+    const {
+      onEditClick,
+      editing,
+      title,
+      onDelClick
+    } = this.props
 
+    if (editing) {
+      return <li onClick={onEditClick}>Edit me here</li>
+    }
     return (
       <li>
-        {task.title} | <a href='#'>Edit</a> | <a onClick={onDelClick} href='#'>Delete</a>
+        {title} | <a onClick={onEditClick} href='#'>Edit</a> | <a onClick={onDelClick} href='#'>Delete</a>
       </li>
     )
   }
 }
 
 Task.propTypes = {
-  task: React.PropTypes.object,
+  title: React.PropTypes.string,
+  id: React.PropTypes.number,
+  onEditClick: React.PropTypes.func,
+  editing: React.PropTypes.bool,
   onDelClick: React.PropTypes.func
 }
 
