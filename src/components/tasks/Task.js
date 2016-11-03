44e451c4f350e8bb38e5c6b8dc4ea9a47e9a494d
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-03 02:48:21 -0400

    Add onDelClick event handler to delete link

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react'
 
 class Task extends Component {
 
-
   render () {
-    const { task } = this.props
+    const { task, onDelClick } = this.props
 
     return (
       <li>
-        {task.title} | <a href='#'>Edit</a> | <a href='#'>Delete</a>
+        {task.title} | <a href='#'>Edit</a> | <a onClick={onDelClick} href='#'>Delete</a>
       </li>
     )
   }
 }
 
 Task.propTypes = {
-  task: React.PropTypes.object
+  task: React.PropTypes.object,
+  onDelClick: React.PropTypes.func
 }
 
 export default Task
