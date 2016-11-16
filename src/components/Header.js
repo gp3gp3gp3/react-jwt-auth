3f39947daeae74eef05432adc04bb58506061e66
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-16 16:00:13 -0500

    Add link in header, remove commented out code

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,41 +39,16 @@ const Logged = (props) => (
     targetOrigin={{horizontal: 'right', vertical: 'top'}}
     anchorOrigin={{horizontal: 'right', vertical: 'top'}}
   >
-    <MenuItem primaryText='Sign out' />
+    <MenuItem
+      primaryText='Sign out'
+      containerElement={<Link to='/signout' />}
+    />
   </IconMenu>
 )
 
 Logged.muiName = 'IconMenu'
 
 class Header extends Component {
-  // renderLinks () {
-  //   if (this.props.authenticated) {
-  //     return (
-  //       <nav className='navbar navbar-light'>
-  //         <Link to='/tasks' className='navbar-brand'>Tasks List</Link>
-  //         <ul className='nav navbar-nav'>
-  //           <li className='nav-item'>
-  //             <Link to='/signout' className='nav-link'>Sign Out</Link>
-  //           </li>
-  //         </ul>
-  //       </nav>
-  //     )
-  //   } else {
-  //     return (
-  //       <nav className='navbar navbar-light'>
-  //         <Link to='/' className='navbar-brand'>Tasks List</Link>
-  //         <ul className='nav navbar-nav'>
-  //           <li className='nav-item'>
-  //             <Link to='/signin' className='nav-link'>Sign In</Link>
-  //           </li>
-  //           <li className='nav-item'>
-  //             <Link to='/signup' className='nav-link'>Sign Up</Link>
-  //           </li>
-  //         </ul>
-  //       </nav>
-  //     )
-  //   }
-  // }
 
   render () {
     const { authenticated } = this.props
